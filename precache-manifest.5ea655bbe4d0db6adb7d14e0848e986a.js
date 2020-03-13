efa50f63e34877e572085779ead2480a35b8c09e
TianJianSir <18721154047@163.com>
2020-03-13 09:42:33 +0000

    Deploying to gh-pages from master a8761f838129d41d3ca85995da9d97e1f0055225

---
+++ precache-manifest.5ea655bbe4d0db6adb7d14e0848e986a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25373f6b7d4333049147eadb7304d458",
+    "url": "http://tianjian.work/site/index.html"
+  },
+  {
+    "revision": "1b132ddc4eaefb45f726",
+    "url": "http://tianjian.work/site/static/css/2.b8d38b07.chunk.css"
+  },
+  {
+    "revision": "eedf42de5e3563c92ab7",
+    "url": "http://tianjian.work/site/static/css/main.8257b0a3.chunk.css"
+  },
+  {
+    "revision": "1b132ddc4eaefb45f726",
+    "url": "http://tianjian.work/site/static/js/2.edd6542c.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "http://tianjian.work/site/static/js/2.edd6542c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eedf42de5e3563c92ab7",
+    "url": "http://tianjian.work/site/static/js/main.5b1bc380.chunk.js"
+  },
+  {
+    "revision": "0792dd3eeef12080a270",
+    "url": "http://tianjian.work/site/static/js/runtime-main.ff8e3a9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "http://tianjian.work/site/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "http://tianjian.work/site/precache-manifest.5ea655bbe4d0db6adb7d14e0848e986a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://tianjian.work/site/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
